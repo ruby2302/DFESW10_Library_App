d65b60cb221b13e6ef691b55a601d6efdec81008
ruby2302 <rubyembley@gmail.com>
2022-03-04 15:33:43 +0000

    DFP-19 Â#comment Created initial project structure and entity

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,5 +1,19 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repo.BookRepo;
+
+@Service
 public class BookService {
+	
+	private BookRepo repo;
+
+	public BookService(BookRepo repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	
 
 }
