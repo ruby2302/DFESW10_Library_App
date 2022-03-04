802abf67094a7db1b32c448bb45296440c4b31b6
ruby2302 <rubyembley@gmail.com>
2022-03-04 13:59:30 +0000

    committing intital folder structure

---
+++ src/main/java/com/example/demo/Dfesw10FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfesw10FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dfesw10FinalProjectApplication.class, args);
+	}
+
+}
