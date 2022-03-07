df6cbedb3eb542e333b6ec56ac30dd75dd55297c
ruby2302 <rubyembley@gmail.com>
2022-03-07 12:20:39 +0000

    DFP-8 #comment added getAll method to controller

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,12 @@ public class BookController {
 	@PostMapping("/addBook")
 	public ResponseEntity<Book> addbook(@RequestBody Book book) {
 		return new ResponseEntity<>(this.service.addBook(book), HttpStatus.CREATED);
+		
+	}
+	
+	@GetMapping("/getAllBooks")
+	public ResponseEntity<List<Book>> getall() {
+		return new ResponseEntity<>(this.service.getAll(), HttpStatus.ACCEPTED);
 		
 	}
 	
