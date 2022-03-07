892171c2ee098c3d30fb4b9493a4b0631b2aa80c
ruby2302 <rubyembley@gmail.com>
2022-03-07 12:35:36 +0000

    DFP-5 #comment added searchBy and findById methods to controller

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,16 @@ public class BookController {
 		
 	}
 	
+	@GetMapping("/findById/{id}")
+	public ResponseEntity<Book> findybyid(@PathVariable Long id) {
+		return new ResponseEntity<>(this.service.findbyId(id), HttpStatus.ACCEPTED);
+		
+	}
+	
+	@GetMapping("/searchLibrary")
+	public ResponseEntity<List<Book>> searchlibrary(@RequestBody Example<Book> book) {
+		return new ResponseEntity<>(this.service.search(book), HttpStatus.ACCEPTED);
+	}
 	
 	
 	
