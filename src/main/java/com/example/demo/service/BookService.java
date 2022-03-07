5a7bb0ae7ad844082bcaa7673c1dac27f8519889
ruby2302 <rubyembley@gmail.com>
2022-03-07 11:55:52 +0000

    DFP-5  #comment added CRUD to service

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,8 +1,16 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Book;
+import com.example.demo.entity.Cat;
 import com.example.demo.repo.BookRepo;
 
 @Service
@@ -19,6 +27,36 @@ public class BookService {
 		return this.repo.save(book);	
 	}
 	
+	public List<Book> getAll() {
+		return this.repo.findAll();
+	}
 	
-
+	public Book findbyId(Long id) {
+		Optional<Book> book = this.repo.findById(id);
+		return book.get();
+	}
+	
+	public List<Book> search(Example<Book> book) {
+		return this.repo.findAll(book, Sort.unsorted());
+		
+	}
+	
+	public Book update(Long id, Book updatedbook) {
+		Book existingBook = this.repo.findById(id).orElseThrow(EntityNotFoundException::new);
+		
+		existingBook.setTitle(updatedbook.getTitle());
+		existingBook.setAuthor(updatedbook.getAuthor());
+		existingBook.setGenre(updatedbook.getGenre());
+		existingBook.setCheckedOut(updatedbook.getCheckedOut());
+		existingBook.setReturnDate(updatedbook.getReturnDate());
+		
+		return this.repo.save(existingBook);
+		
+	}
+	
+	public Boolean delete(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+	
 }
