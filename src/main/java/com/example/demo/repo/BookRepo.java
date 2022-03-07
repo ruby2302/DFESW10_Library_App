b172c40131806497f407c30586b0542666006f28
ruby2302 <rubyembley@gmail.com>
2022-03-07 16:00:51 +0000

    DFP-5 #comment fixed search method

--- src/main/java/com/example/demo/repo/BookRepo.java
+++ src/main/java/com/example/demo/repo/BookRepo.java
@@ -1,11 +1,13 @@
 package com.example.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Book;
 
 @Repository
-public interface BookRepo extends JpaRepository<Book, Long > {
+public interface BookRepo extends JpaRepository<Book, Long>,
+	QueryByExampleExecutor<Book> {}
 
-}
+
